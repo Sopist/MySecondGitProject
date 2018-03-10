a5a3ac03ad1e3bd0e5144975a89de418b4988b56
sunsilkky@gmail.com <sunsilkky@gmail.com>
2018-03-10 12:21:37 -0500

    added print statement

--- src/main/GoodMorningClass.java
+++ src/main/GoodMorningClass.java
@@ -3,6 +3,7 @@ package main;
 public class GoodMorningClass {
 	public static void main(String[] args) {		
 		System.out.println("Good Morning Class!");		
+		System.out.println("My name is Sopist");
 	}
 
 }
